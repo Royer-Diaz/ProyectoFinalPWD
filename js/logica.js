7b9ec6fac2e004e377587da456a13c590c2ee329
Royer Diaz <royer.diaz@outlook.com>
2015-04-11 13:31:22 -0600

    Agregue el primer movimiento de pantalla

--- js/logica.js
+++ js/logica.js
@@ -1,4 +1,39 @@
 
 
 
+//Vuelve a la Pantalla Principal
+var volverPantallaInicio = function(){
+  jQuery('#slider').animate({'margin-left': '-1300px'}, 500);
+};
 
+
+var moverDetallePersonaje = function(){
+   $('#slider').animate({'margin-left': '-2600px'}, 500);
+};
+
+
+
+
+
+//-------Event Listeners---------//
+
+$(document).ready(function(){
+//Botones de la interfas grafica:
+
+  //Boton de Volver a Pantalla principal 
+  $('.volver').click(function(){
+        volverPantallaInicio();
+    });
+
+   //Boton de resetar el formulario
+    $('#resetear').click(function(){
+      $('.has-error').removeClass('has-error');
+      resetearInputs();
+    }); 
+
+
+    $('.move').click(function(){  
+      moverDetallePersonaje();
+    })
+
+});
